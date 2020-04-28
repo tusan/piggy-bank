297bedd698def0d9d1ba8d87f89e3a5f6b3f4ca9
Santolo Tubelli <santolo.tubelli@jobrapido.com>
2020-04-28 17:00:01 +0200

    Used certificate to generate JWT authentication key

--- src/main/java/com/piggybank/security/SecurityConfigs.java
+++ src/main/java/com/piggybank/security/SecurityConfigs.java
@@ -1,27 +1,34 @@
 package com.piggybank.security;
 
 import com.piggybank.security.filters.JwtAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.security.Key;
+import java.security.KeyStore;
+
+import static com.piggybank.config.Environment.JWT_KEY_ALIAS;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfigs extends WebSecurityConfigurerAdapter {
-  private final AuthenticationManager authenticationManager;
-  private final SecurityContextHolderFacade securityContextHolderFacade;
+  @Autowired private AuthenticationManager authenticationManager;
+  @Autowired private SecurityContextHolderFacade securityContextHolderFacade;
 
-  public SecurityConfigs(
-      final AuthenticationManager authenticationManager,
-      final SecurityContextHolderFacade securityContextHolderFacade) {
-    this.authenticationManager = authenticationManager;
-    this.securityContextHolderFacade = securityContextHolderFacade;
-  }
+  @Value("${security.keystore.location}")
+  private Resource keystore;
+
+  @Value("${security.keystore.password}")
+  private String keystorePassword;
 
   @Override
   public void configure(WebSecurity web) {
@@ -45,5 +52,13 @@ class SecurityConfigs extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .anyRequest()
         .authenticated();
+  }
+
+  @Bean(name = "jwtKey")
+  public Key jwtKey() throws Exception {
+    final KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+    ks.load(keystore.getInputStream(), keystorePassword.toCharArray());
+
+    return ks.getKey(JWT_KEY_ALIAS, keystorePassword.toCharArray());
   }
 }
