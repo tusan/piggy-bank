f817c8261807f82a22a44fc0aabef84caa44dd7e
Santolo Tubelli <santolo.tubelli@jobrapido.com>
2020-05-03 23:35:52 +0200

    Removed unused method

--- src/main/java/com/piggybank/service/users/repository/JpaUserRepository.java
+++ src/main/java/com/piggybank/service/users/repository/JpaUserRepository.java
@@ -6,6 +6,4 @@ import java.util.Optional;
 
 public interface JpaUserRepository extends JpaRepository<PiggyBankUser, String> {
   Optional<PiggyBankUser> findByUsername(String username);
-
-  Optional<PiggyBankUser> findByToken(String token);
 }
